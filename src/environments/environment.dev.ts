b4125967a7e2447a77aa5d5d38122bb92ed60aa8
Agattepaille <amelie.gattepaille@gmail.com>
2026-02-08 18:48:55 +0100

    Update development environment API URL

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'crossfit-frontend-vjlhzi-38aba5-141-94-77-150.traefik.me',
+  apiUrl: 'https://crossfit-backend-mvz7ah-c87a5f-141-94-77-150.traefik.me/',
 };
